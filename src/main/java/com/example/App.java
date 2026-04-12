a8671ebfc75bc5c21a6ae98c7e5fa5fa21b73aee
Karthik-M-R <karthikmr135@gmail.com>
2026-04-12 17:03:16 +0530

    Jenkins

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,17 +3,24 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.lang.Thread;
+
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.saucedemo.com/");
+        driver.get("https://practicetestautomation.com/practice-test-login/");
         driver.manage().window().maximize();
 
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+        
+        driver.findElement(By.id("submit")).click();
+        
+        Thread.sleep(5000);
+        
+         driver.quit();
     }
 }
